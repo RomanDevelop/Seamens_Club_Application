35de3d5ec15c1aefcd191d538452a4b28ca55859
roman.k <roman.k@3sda-evayadatasystems.com>
2024-01-14 13:44:38 +0100

    updated max to 14 jan

--- lib/services/app_service.dart
+++ lib/services/app_service.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:flutter/material.dart';
-import 'package:flutter_html/flutter_html.dart';
 import 'package:flutter_web_browser/flutter_web_browser.dart';
 import 'package:html/parser.dart';
 import 'package:html_unescape/html_unescape.dart';
@@ -12,10 +11,10 @@ import 'package:reading_time/reading_time.dart';
 import 'package:url_launcher/url_launcher.dart' as urlLauncher;
 import 'package:news_app/utils/toast.dart';
 import 'package:provider/provider.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 
 class AppService {
-
   Future<bool?> checkInternet() async {
     bool? internet;
     try {
@@ -30,24 +29,22 @@ class AppService {
     }
     return internet;
   }
-  
+
   Future openLink(context, String url) async {
     final uri = Uri.parse(url);
     if (await urlLauncher.canLaunchUrl(uri)) {
-      urlLauncher.launchUrl(uri);
+      urlLauncher.launchUrl(uri, mode: LaunchMode.externalApplication);
     } else {
       openToast1(context, "Can't launch the url");
     }
   }
 
-  
-
   Future openEmailSupport(context) async {
-
     final Uri uri = Uri(
       scheme: 'mailto',
       path: Config().supportEmail,
-      query: 'subject=About ${Config().appName}&body=', //add subject and body here
+      query:
+          'subject=About ${Config().appName}&body=', //add subject and body here
     );
 
     if (await urlLauncher.canLaunchUrl(uri)) {
@@ -57,33 +54,30 @@ class AppService {
     }
   }
 
-
-
-
   Future openLinkWithCustomTab(BuildContext context, String url) async {
-    try{
+    try {
       await FlutterWebBrowser.openWebPage(
-      url: url,
-      customTabsOptions: CustomTabsOptions(
-        colorScheme: context.read<ThemeBloc>().darkTheme! ? CustomTabsColorScheme.dark : CustomTabsColorScheme.light,
-        //addDefaultShareMenuItem: true,
-        instantAppsEnabled: true,
-        showTitle: true,
-        urlBarHidingEnabled: true,
-      ),
-      safariVCOptions: SafariViewControllerOptions(
-        barCollapsingEnabled: true,
-        dismissButtonStyle: SafariViewControllerDismissButtonStyle.close,
-        modalPresentationCapturesStatusBarAppearance: true,
-      ),
-    );
-    }catch(e){
+        url: url,
+        customTabsOptions: CustomTabsOptions(
+          colorScheme: context.read<ThemeBloc>().darkTheme!
+              ? CustomTabsColorScheme.dark
+              : CustomTabsColorScheme.light,
+          //addDefaultShareMenuItem: true,
+          instantAppsEnabled: true,
+          showTitle: true,
+          urlBarHidingEnabled: true,
+        ),
+        safariVCOptions: SafariViewControllerOptions(
+          barCollapsingEnabled: true,
+          dismissButtonStyle: SafariViewControllerDismissButtonStyle.close,
+          modalPresentationCapturesStatusBarAppearance: true,
+        ),
+      );
+    } catch (e) {
       openToast1(context, 'Cant launch the url');
       debugPrint(e.toString());
     }
   }
-
-
 
   Future launchAppReview(context) async {
     final SignInBloc sb = Provider.of<SignInBloc>(context, listen: false);
@@ -93,26 +87,22 @@ class AppService {
         writeReview: false);
     if (Platform.isIOS) {
       if (Config().iOSAppId == '000000') {
-        openToast1(context, 'The iOS version is not available on the AppStore yet');
+        openToast1(
+            context, 'The iOS version is not available on the AppStore yet');
       }
     }
   }
 
-
-  static getYoutubeVideoIdFromUrl (String videoUrl){
+  static getYoutubeVideoIdFromUrl(String videoUrl) {
     return YoutubePlayer.convertUrlToId(videoUrl, trimWhitespaces: true);
   }
 
-  static getNormalText (String text){
+  static getNormalText(String text) {
     return HtmlUnescape().convert(parse(text).documentElement!.text);
   }
 
-  static getReadingTime (String text){
+  static getReadingTime(String text) {
     var reader = readingTime(getNormalText(text));
     return reader.msg;
   }
-
-  static CustomRenderMatcher videoMatcher() => (context) => context.tree.element?.localName == 'video';
-  static CustomRenderMatcher iframeMatcher() => (context) => context.tree.element?.localName == 'iframe';
-  static CustomRenderMatcher imageMatcher() => (context) => context.tree.element?.localName == 'img';
 }
